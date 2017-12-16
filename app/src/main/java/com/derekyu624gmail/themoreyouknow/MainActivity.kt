85521d204df99b8ece1f7d8f7f02df6435e609e3
Derek Yu <derekyu624@hotmail.com>
2017-12-15 22:13:03 -0500

    Added fragments to change the content page (Space, Human Body, Sources)

--- app/src/main/java/com/derekyu624gmail/themoreyouknow/MainActivity.kt
+++ app/src/main/java/com/derekyu624gmail/themoreyouknow/MainActivity.kt
@@ -3,6 +3,7 @@ package com.derekyu624gmail.themoreyouknow
 import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.design.widget.NavigationView
+import android.support.v4.app.Fragment
 import android.support.v4.view.GravityCompat
 import android.support.v7.app.ActionBarDrawerToggle
 import android.support.v7.app.AppCompatActivity
@@ -10,29 +11,24 @@ import android.util.TypedValue
 import android.view.View
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Space
 import android.widget.TextView
+import android.widget.Toolbar
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.app_bar_main.*
 
 class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
-    private lateinit var textView: TextView
-    private var databaseAccess: DatabaseAccess? = null
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //Set the fragment initially
+        var spaceFrag = SpaceFragment()
+        var fragmentTransaction: android.support.v4.app.FragmentTransaction = supportFragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.fragment_container, spaceFrag)
+        fragmentTransaction.commit()
+
         setSupportActionBar(toolbar)
-        this.textView = findViewById(R.id.factArea)
-
-        databaseAccess = DatabaseAccess.getInstance(this)
-        databaseAccess?.open()
-
-        /*
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
-        }
-        */
         val toggle = ActionBarDrawerToggle(
                 this, drawer_layout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
         drawer_layout.addDrawerListener(toggle)
@@ -74,26 +70,21 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         // Handle navigation view item clicks here.
         when (item.itemId) {
 
-            R.id.nav_gallery -> {
-
+            R.id.nav_space -> {
+                var spaceFrag = SpaceFragment()
+                var fragmentTransaction: android.support.v4.app.FragmentTransaction = supportFragmentManager.beginTransaction()
+                fragmentTransaction.replace(R.id.fragment_container, spaceFrag)
+                fragmentTransaction.commit()
             }
-            R.id.nav_slideshow -> {
-
-            }
-            R.id.nav_manage -> {
-
+            R.id.nav_sources -> {
+                var sourcesFrag= SourcesFragment()
+                var fragmentTransaction: android.support.v4.app.FragmentTransaction = supportFragmentManager.beginTransaction()
+                fragmentTransaction.replace(R.id.fragment_container, sourcesFrag)
+                fragmentTransaction.commit()
             }
         }
 
         drawer_layout.closeDrawer(GravityCompat.START)
         return true
-    }
-
-
-    fun onButtonTap(v: View) {
-        this.textView.setText(null)
-        val fact: String = databaseAccess!!.getFact("space")
-        this.textView.setText(fact)
-
     }
 }
